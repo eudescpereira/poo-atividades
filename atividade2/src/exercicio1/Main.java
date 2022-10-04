905b277c32e1f5aa8364e2c7643ccab609db7ce8
Eudes Pereira <99928327+eudescpereira@users.noreply.github.com>
2022-10-04 18:15:32 -0300

    Update Main.java

--- atividade2/src/exercicio1/Main.java
+++ atividade2/src/exercicio1/Main.java
@@ -4,25 +4,25 @@ public class Main {
 
 	public static void main(String[] args) {
 				
-				Soma s = new Soma();
-				s.setValor1(10);
-				s.setValor2(5);
-				s.Calcula();
+		Soma s = new Soma();
+		s.setValor1(10);
+		s.setValor2(5);
+		s.Calcula();
 				
-				Subtracao sub = new Subtracao();
-				sub.setValor1(10);
-				sub.setValor2(5);
-				sub.Calcula();
+		Subtracao sub = new Subtracao();
+		sub.setValor1(10);
+		sub.setValor2(5);
+		sub.Calcula();
+		
+		Multiplicacao mult = new Multiplicacao();
+		mult.setValor1(10);
+		mult.setValor2(5);
+		mult.Calcula();
 				
-				Multiplicacao mult = new Multiplicacao();
-				mult.setValor1(10);
-				mult.setValor2(5);
-				mult.Calcula();
-				
-				Divisao div = new Divisao();
-				div.setValor1(10);
-				div.setValor2(5);
-				div.Calcula();
+		Divisao div = new Divisao();
+		div.setValor1(10);
+		div.setValor2(5);
+		div.Calcula();
 	}
 
 }
