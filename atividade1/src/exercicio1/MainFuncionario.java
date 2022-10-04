1d2c0fd110845cb3d898fd64dabf85b950932232
Eudes Pereira <99928327+eudescpereira@users.noreply.github.com>
2022-10-04 17:08:32 -0300

    Update MainFuncionario.java

--- atividade1/src/exercicio1/MainFuncionario.java
+++ atividade1/src/exercicio1/MainFuncionario.java
@@ -5,7 +5,9 @@ public class MainFuncionario {
 		public static void main(String[] args) {
 			
 			Funcionario f = new Funcionario("Eudes Pereira", 3000);
-			System.out.println("Dados do Funcionário\n");
+			
+			System.out.println("Dados do Funcionario\n");
+			
 			f.exibeDados();
 			
 			f.addAumento(1000);
@@ -17,6 +19,7 @@ public class MainFuncionario {
 			Assistente a = new Assistente("Samara", 2000, 1);
 			
 			System.out.println("\nDados do Assistente\n");
+			
 			a.exibeDados();
 			
 			a.addAumento(500);
@@ -27,12 +30,14 @@ public class MainFuncionario {
 			
 			AssistenteTecnico aT = new AssistenteTecnico("Eduarda", 2500, 2, 500);
 			
-			System.out.println("\nDados do Assistente Técnico\n");
+			System.out.println("\nDados do Assistente Tecnico\n");
+			
 			aT.exibeDados();
 			
 			AssistenteAdministrativo aA = new AssistenteAdministrativo("Bruna", 3000, 3, "noite");
 			
 			System.out.println("\nDados do Assistente Administrativo\n");
+			
 			aA.exibeDados();
 		
 		}
